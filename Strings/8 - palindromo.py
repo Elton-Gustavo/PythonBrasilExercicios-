d55d4296ca47b79deb61c9a932f2a8cfd2f115ee
Elton Gustavo <97633592+Elton-Gustavo@users.noreply.github.com>
2022-07-19 14:09:49 -0300

    Add files via upload

---
+++ Strings/8 - palindromo.py
@@ -0,0 +1,10 @@
+frase = input("Digite algo para verificar se é um palindromo: ").strip().split()
+resultado = ''
+for x in range(0, len(frase)):
+    resultado += frase[x]
+if resultado[::] == resultado[::-1]:
+    print(f'{resultado[::-1].upper()} é um palindromo de {resultado[::].upper()}')
+else:
+    print(f'{resultado[::-1].upper()} não é um palindromo de {resultado[::].upper()}')
+    
+
